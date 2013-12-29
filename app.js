b826146735f649465da9a0fbd81391f28ed5568f
Aditya Patadia <aditya@patadia.org>
2013-12-30 04:04:04 +0530

    Simple prototype ready.

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 var express = require('express')
-  , app = express()
-  , http = require('http')
-  , server = http.createServer(app)
-  , io = require('socket.io').listen(server);
+, app = express()
+, http = require('http')
+, server = http.createServer(app)
+, io = require('socket.io').listen(server)
+, sqlite3 = require('sqlite3');
 
 server.listen(8080);
 
+var db = new sqlite3.Database('livenote.sqlite3'); 
+// db.run("CREATE TABLE notes (id TEXT PRIMARY KEY, note TEXT)",function(err){
+//  console.log(err);
+// });
+//db.run("insert into notes (id,note) values ('ddd','This is note.')")
+
+
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  socket.on('getNote', function (data) {
+    db.get("select id,note from notes where id = '"+data.id+"'",function(err,row){
+      if(row){
+        socket.emit('setNote', { note: row.note });
+      } else {
+        socket.emit('setNote', { note: "" });
+      }
+    //res.send(row.note);
+    });
   });
+  socket.on("changeNote",function(data){
+    socket.broadcast.emit('changeBackNote', data);
+  })
 });
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
+
+app.get('/:id', function (req, res) {
+  res.sendfile(__dirname + '/notes.html');
+//  res.send(req.params.id);
+});
+
 app.use("/public", express.static(__dirname + "/public"));
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,26 @@
 $(function() {
-	$("#create").attr("href",new Date().valueOf().toString(16));
+	$("#note").keyup(function(e){
+		socket.emit('changeNote', { note : $(this).val() });
+	});
 });
 
-var socket = io.connect('http://localhost');
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
+var socket = io.connect();
+
+socket.emit('getNote', { id: document.location.href.split("/").pop()});
+
+socket.on("setNote",function(data){
+	// $("#note").off("change");
+	// $("#note").val(data.note);
+	// $("#note").on("change",function(e){
+	// 	socket.emit('changeNote', { note : $(this).val() });
+	// });
+});
+
+socket.on("changeBackNote",function(data){
+	// $("#note").off("change");
+	// console.log(data);
+	$("#note").val(data.note);
+	// $("#note").on("change",function(e){
+	// 	socket.emit('changeNote', { note : $(this).val() });
+	// });
 });
