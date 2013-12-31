f5dfe27dde8b1c275520dbfb6404f7a1cf19138e
Aditya Patadia <aditya@patadia.org>
2014-01-01 02:27:56 +0530

    database save removed.

--- app.js
+++ app.js
@@ -18,13 +18,12 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 8000
 
 server.listen(port,ip);
 
+var livenotes = new Object();
 var databaseLoc = (process.env.OPENSHIFT_DATA_DIR)?process.env.OPENSHIFT_DATA_DIR+"livenote.sqlite3" : "livenote.sqlite3";
 var db = new sqlite3.Database(databaseLoc); 
-var livenotes = new Object();
 db.run("CREATE TABLE notes (id TEXT PRIMARY KEY, note TEXT)",function(err){
   console.log(err);
 });
-//db.run("insert into notes (id,note) values ('ddd','This is note.')")
 
 
 io.sockets.on('connection', function (socket) {
@@ -45,16 +44,16 @@ io.sockets.on('connection', function (socket) {
 
   socket.on("changeNote",function(data){
     socket.broadcast.to(data.id).emit('changeBackNote', data);
-    var newval = livenotes[data.id];
-    var op = data.op;
-    if(op.d!==null) {
-      newval = newval.slice(0,op.p)+newval.slice(op.p+op.d);
-    }
-    if(op.i!==null){
-      newval = newval.insert(op.p,op.i);
-    } 
-    livenotes[data.id] = newval;
-    db.run("INSERT OR REPLACE INTO notes ('id', 'note') VALUES (?,?)",[data.id,encodeURIComponent(newval)]);
+    // var newval = livenotes[data.id];
+    // var op = data.op;
+    // if(op.d!==null) {
+    //   newval = newval.slice(0,op.p)+newval.slice(op.p+op.d);
+    // }
+    // if(op.i!==null){
+    //   newval = newval.insert(op.p,op.i);
+    // } 
+    // livenotes[data.id] = newval;
+    // db.run("INSERT OR REPLACE INTO notes ('id', 'note') VALUES (?,?)",[data.id,encodeURIComponent(newval)]);
   });
 
   socket.on("disconnect",function(){
