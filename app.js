3662fa9395d004ed25780738f7f6c5217ab97927
Aditya Patadia <aditya@patadia.org>
2014-04-27 22:57:22 +0530

    app options changes.

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-var express = require('express')
-, app = express()
-, server = require('http').Server(app)
-, io = require('socket.io')(server)
-, compress = require('compression')()
-, sqlite3 = require('sqlite3');
-
-if(process.env.OPENSHIFT_NODEJS_PORT){
-  io.enable('browser client minification');  // send minified client
-  io.enable('browser client etag');          // apply etag caching logic based on version number
-  io.enable('browser client gzip');          // gzip the file
-  io.set('log level', 1);                    // reduce logging
-  io.set('transports', ['websocket']);
-}
+var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server,  {'transports' : ['websocket']} ),
+    compress = require('compression')(),
+    sqlite3 = require('sqlite3');
 
 app.use(compress);
 app.disable('x-powered-by');
