730dfffa88fa7d7a3cd35c58869727f60765226c
Aditya Patadia <aditya@patadia.org>
2014-04-28 00:14:33 +0530

    header changed.

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use(compress);
 app.disable('x-powered-by');
 
 app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8000');
+
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         next();
